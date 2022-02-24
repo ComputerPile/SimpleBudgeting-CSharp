22df2e05f7725537553f6e6f4d7c26675044622d
root <root@Luna.localdomain>
2022-02-24 15:42:38 -0500

    Initial commit

---
+++ SimpleBudgeting/Library.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+
+public class Library
+{
+    // Prompt question
+    // This function prompts the user to enter a string value
+    // then AskInfo will check to make sure the string value
+    // can be converted to a float value
+    public float AskInfo(string prompt)
+    {
+        bool isNumber = true; // Check if input is a number
+        float paymentAmount = 0;
+
+
+        Console.Write("\nEnter {0} amount: ", prompt);
+
+        do
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                paymentAmount = float.Parse(input);
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a number!");
+                Console.ResetColor();
+
+                // Sleep for 2 seconds for dramatic effect
+                Thread.Sleep(2000);
+
+                Console.Write("Amount: ");
+
+                isNumber = false;
+            }
+        } while (!isNumber);
+
+        return paymentAmount;
+    }
+
+    // Prompt question, but with new bills
+    // must input question as string
+    public float AddBill(float total, string bill)
+    {
+        bool isNumber = true; // check if input is a number as before
+        float paymentAmount = 0;
+
+        Console.Write("\nEnter {0} amount: ", bill);
+
+        do
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                paymentAmount = float.Parse(input);
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a number!");
+                Console.ResetColor();
+
+                // Sleep for 2 seconds for dramatic effect
+                Thread.Sleep(2000);
+
+                Console.Write("Amount: ");
+
+                isNumber = false;
+            }
+        } while (!isNumber);
+
+        //float remainderAmount = total - paymentAmount;
+
+        return total - paymentAmount;
+    }
+
+    // This throws a message to the user when they
+    // input an invalid message.
+    // Also it sleeps for 2 seconds and returns false boolean.
+    public bool ThrowError(string errorMessage)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(errorMessage);
+        Console.ResetColor();
+
+        // Sleep for 2 seconds for dramatic effect
+        Thread.Sleep(2000);
+
+        // Return true for error thrown
+        return true;  
+    }
+}
---
+++ SimpleBudgeting/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace SimpleBudgeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // My Library Class
+            Library myLib = new Library();
+            
+            // Verision
+            Console.WriteLine("Simple Budgeting Console Edition v1.0.0\n");
+            //Console.WriteLine("Hint: Type \"exit\" to end this program.");
+
+            // Variable for total amount
+            float total = 0;
+
+            // Array of bills feel free to edit with your own bills
+            string[] bills =
+            {
+                "your monthly salary",
+                "car payment",
+                "car insurance",
+                "electric bill",
+                "water bill",
+                "internet"
+            };
+
+            // More bills can be added by user later
+
+            foreach (string bill in bills)
+            {
+                if (bill == "your monthly salary")
+                {
+                    total = myLib.AskInfo(bill);
+
+                    Console.WriteLine("Total amount starting: $ {0}", total.ToString("0.00"));
+                }
+                else
+                {
+                    float paymentAmount = myLib.AskInfo(bill);
+
+                    total -= paymentAmount;
+
+                    Console.WriteLine("\nTotal remaining: $ {0}", total);
+                }
+            }
+
+            // Just to add some space
+            Console.WriteLine("------------------------------");
+
+            bool addError = false;
+            //bool addBills;
+
+            do
+            {
+                Console.Write("Would you like to deduct more bills or payments? (Y/n): ");
+                string addBillsInput = Console.ReadLine().ToUpper();
+
+                if (addBillsInput == "Y")
+                {
+                    addError = false;
+                }
+                else if (addBillsInput == "N")
+                {
+                    Environment.Exit(0); // exit program
+                }
+                else
+                {
+                    addError = myLib.ThrowError("Please enter Y or N.");
+                }
+            } while (addError);
+
+            Console.WriteLine("So... you've made it this far...");
+
+            // For adding more bills and deducting from your total amount
+            bool addMoreBills = true;
+
+            do
+            {
+                float payment = 0; // Value to deduct from total
+
+                // Get Name of bill
+                Console.Write("\nEnter a bill name: ");
+                string billName = Console.ReadLine();
+
+                // Add bool for checking for error and loop
+                bool error = false;
+
+                Console.Write("Enter {0} amount: ", billName);
+
+                do
+                {
+                    try
+                    {
+                        payment = float.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        error = myLib.ThrowError("\nPlease enter a number!");
+
+                        Console.Write("Amount: ");
+                    }
+                } while (error);
+
+                // Subtract from total
+                total -= payment;
+
+                Console.WriteLine("\nTotal remaining: $ {0}", total);
+
+                // Ask user if they want to add more
+                bool addMoreError = false;
+
+                do
+                {
+                    Console.Write("\nWould you like to add more? (Y/n)");
+                    string addMoreInput = Console.ReadLine().ToUpper();
+
+                    if (addMoreInput == "N")
+                    {
+                        addMoreBills = false;
+                        addMoreError = false;
+                    }
+                    else if (addMoreInput == "Y")
+                    {
+                        addMoreBills = true;
+                        addMoreError = false;
+                    }
+                    else
+                    {
+                        addMoreError = myLib.ThrowError("\nPlease enter Y or N.");
+                    }
+                } while (addMoreError);
+
+            } while (addMoreBills);
+        }
+    }
+}
+/* David says Documentation is key! */
